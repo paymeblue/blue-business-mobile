3e30b989ec20d203e85998c9c886ee04e607e00e
Priscilla Abhulimen <priscillaabhulimen@gmail.com>
2024-05-24 12:09:04 +0100

    "Removed imports and commented out code in DashboardShellViewModel"

--- lib/modules/dashboard_shell/presentation/view_model.dart
+++ lib/modules/dashboard_shell/presentation/view_model.dart
@@ -2,12 +2,8 @@ import 'package:blue_business/core/extensions.dart';
 import 'package:blue_business/core/io/api/timed_refresh.dart';
 import 'package:blue_business/core/module_config/base_view_model.dart';
 import 'package:blue_business/core/navigation/route_names.dart';
-import 'package:blue_business/core/services/locator.dart';
-import 'package:blue_business/core/utils/constants.dart';
-import 'package:blue_business/widgets/modals/notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:provider/provider.dart';
 
 class DashboardShellViewModel extends BaseViewModel {
   late Size size;
@@ -36,14 +32,7 @@ class DashboardShellViewModel extends BaseViewModel {
         context.push(RoutePaths.homePath);
       case 1:
         context.push(RoutePaths.insightsPath);
-      case 2:
-      // if (locator<AppStateValues>().wallet == null) {
-      //   AppNotification.warning(message: "Getting wallet data...");
-      // } else {
-      //   Provider.of<AppStateValues>(context, listen: false).hasNewMessage =
-      //       false;
-      //   context.push(RoutePaths.messagePath);
-      // }
+
       case 3:
         context.push(RoutePaths.billsPath);
       case 4:
