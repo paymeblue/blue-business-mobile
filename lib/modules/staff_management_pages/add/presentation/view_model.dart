2ec70f22b1f8d045760b1646b1aacde5c35a4f04
Priscilla Abhulimen <priscillaabhulimen@gmail.com>
2024-06-19 11:42:57 +0100

    "Added branch dropdown feature with infinite scroll and search functionality to AddStaffViewModel and BlueDropdown widget."

--- lib/modules/staff_management_pages/add/presentation/view_model.dart
+++ lib/modules/staff_management_pages/add/presentation/view_model.dart
@@ -1,15 +1,22 @@
 import 'package:blue_business/core/extensions.dart';
 import 'package:blue_business/core/gen/colors.gen.dart';
+import 'package:blue_business/core/io/api/branch_service/branch_service.dart';
 import 'package:blue_business/core/io/api/country_code.dart';
+import 'package:blue_business/core/io/api/dio_config.dart';
+import 'package:blue_business/core/models/branches/branch.dart';
+import 'package:blue_business/core/models/branches/get/response/get_branches_response.dart';
 import 'package:blue_business/core/models/country/country_code.dart';
 import 'package:blue_business/core/module_config/base_view_model.dart';
 import 'package:blue_business/core/navigation/route_names.dart';
+import 'package:blue_business/core/services/locator.dart';
+import 'package:blue_business/core/utils/constants.dart';
 import 'package:blue_business/core/utils/error_handler.dart';
 import 'package:blue_business/widgets/modals/dialogs.dart';
 import 'package:blue_business/widgets/modals/notifications.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:go_router/go_router.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
 class AddStaffViewModel extends BaseViewModel {
   late Size size;
@@ -18,6 +25,9 @@ class AddStaffViewModel extends BaseViewModel {
     size = context.mediaQuery.size;
 
     setSelectedCountry();
+    branchPagingController.addPageRequestListener((pageKey) {
+      getBranches(pageKey);
+    });
   }
 
   goBack(BuildContext context) {
@@ -131,5 +141,39 @@ class AddStaffViewModel extends BaseViewModel {
     }
 
     return selectedCountry!.dialCode + number;
+  }
+
+  PagingController<int, Branch> branchPagingController =
+      PagingController<int, Branch>(firstPageKey: 1);
+
+  getBranches(int page) async {
+    try {
+      GetBranchesResponse response = await BranchService(
+              DioConfig.dio(locator<AppStateValues>().accessToken))
+          .getAllBranches(
+            page: page,
+            limit: 50,
+            search:
+                searchController.text.isEmpty ? null : searchController.text,
+          )
+          .onError(
+            (error, stackTrace) => GetBranchesResponse(
+                message: AppErrorHandler.getErrorMessage(error)),
+          );
+
+      if (response.status == "success") {
+        if (response.data!.loadMore) {
+          branchPagingController.appendPage(response.data!.data, page + 1);
+        } else {
+          branchPagingController.appendLastPage(response.data!.data);
+        }
+
+        notifyListeners();
+      } else {
+        branchPagingController.error = response.message;
+      }
+    } catch (e) {
+      branchPagingController.error = AppErrorHandler.getErrorMessage(e);
+    }
   }
 }
