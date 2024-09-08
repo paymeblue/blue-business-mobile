5ba9f51667c52d96d10f409e4612fc99d03cfae7
Priscilla Abhulimen <priscillaabhulimen@gmail.com>
2024-09-08 11:10:32 +0100

    "Changed MainActivity to extend FlutterFragmentActivity instead of FlutterActivity and added configureFlutterEngine method."

--- android/app/src/main/kotlin/com/roman_dev/blue_business/MainActivity.kt
+++ android/app/src/main/kotlin/com/roman_dev/blue_business/MainActivity.kt
@@ -2,5 +2,8 @@ package com.roman_dev.blue_business
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine)
+    }
 }
